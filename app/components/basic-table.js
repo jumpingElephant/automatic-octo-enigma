88093579246486409f6221d3312e7b425168559e
Alexander Rettner <alexander.rettner@googlemail.com>
2017-05-09 20:21:31 +0200

    started with simple table

--- app/components/basic-table.js
+++ app/components/basic-table.js
@@ -1,23 +1,60 @@
 import Ember from 'ember';
-import Columns from '../mixins/columns';
-import layout from '../templates/components/basic-table';
-import { Table } from 'ember-semantic-ui-table';
+import Row from '../components/classes/row';
+import Column from '../components/classes/column';
 
-export default Ember.Component.extend(Columns, {
-  layout,
+export default Ember.Component.extend({
+  store: Ember.inject.service(),
+  isLoading: false,
+  rows: [],
+  selectedRows: [],
+  page: 1,
 
-  rows: Ember.computed(function() {
-    return [{
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'john.doe@company.com',
-      company: 'Foo Bar Company',
-      address: '3184 5th Avenue',
-      country: 'Brazil'
-    }];
+  init() {
+    this._super(...arguments);
+    this.fetchRecords();
+  },
+
+  fetchRecords() {
+    this.set('isLoading', true);
+    return this.get('store').findRecord('automobile', this.automobileId)
+      .then((records) => {
+        var bills = records.get('bills');
+        var rows = bills.map(function(item) {
+          return Row.create(item);
+        });
+        this.set('rows', rows);
+      }).finally(() => {
+        this.set('isLoading', false);
+      });
+  },
+
+  columns: Ember.computed(function() {
+    var cols = [{
+        label: 'Datum',
+        valuePath: 'date'
+      }, {
+        label: 'Kilometerstand',
+        valuePath: 'mileage'
+      }]
+      .map((item) => {
+        var col = Column.create(item);
+        return col;
+      });
+    return cols;
   }),
 
-  table: Ember.computed('columns', 'rows', function() {
-    return new Table(this.get('columns'), this.get('rows'));
-  })
+  totalNumberOfRows: Ember.computed('rows', function() {
+    return this.get('rows').length;
+  }),
+
+  actions: {
+    selectRow(e, row /* , table */ ) {
+      row.toggleProperty('selected');
+    },
+
+    navigateToPage(pageNumber) {
+      this.set('page', pageNumber);
+      this.fetchRecords();
+    }
+  }
 });
---
+++ app/components/classes/column.js
@@ -0,0 +1,132 @@
+import Ember from 'ember';
+
+const { computed } = Ember;
+
+/**
+ * @class Column
+ * @extends Ember.Object
+ * @namespace SemanticUI
+ */
+export default Ember.Object.extend({
+  /**
+   * Label for the column header.
+   *
+   * @property label
+   * @type String
+   * @required
+   */
+  label: '',
+
+  /**
+   * Path to the value displayed in this column.
+   *
+   * @property label
+   * @type String
+   */
+  valuePath: null
+    /*,
+
+      // /**
+      //  * Type of the header for this column. The type is mapped to a component
+      //  * name prefixed with `table-header/`. To create custom header types, you
+      //  * can create a new component named `table-header/my-custom-header` and
+      //  * specify it here as `headerType: 'my-custom-header'`.
+      //  *
+      //  * @property headerType
+      //  * @type String
+      //  * @default 'default'
+      //  */
+    // headerType: 'default',
+
+  // /**
+  //  * Type of the cells for this column. The type is mapped to a component name
+  //  * prefixed with `table-cell/`. To create custom cell types, you can create
+  //  * a new component named `table-cell/my-custom-cell` specify it here as
+  //  * `cellType: 'my-custom-cell'`.
+  //  *
+  //  * @property cellType
+  //  * @type String
+  //  * @default 'default'
+  //  */
+  // cellType: 'default',
+
+  // /**
+  //  * Whether the column can be hidden.
+  //  *
+  //  * @property hideable
+  //  * @type Boolean
+  //  * @default true
+  //  */
+  // hideable: true,
+
+  // /**
+  //  * Whether the column is hidden.
+  //  *
+  //  * @property hidden
+  //  * @type Boolean
+  //  * @default false
+  //  */
+  // hidden: false,
+
+  // *
+  //  * Whether the column is visible.
+  //  *
+  //  * @property visible
+  //  * @type Boolean
+  //  * @default true
+
+  // visible: computed('hidden', {
+  //   get() {
+  //     return !this.get('hidden');
+  //   },
+  //   set(name, value) {
+  //     this.set('hidden', !value);
+  //   }
+  // }),
+
+  // /**
+  //  * Whether the column can be sorted.
+  //  *
+  //  * @property sortable
+  //  * @type Boolean
+  //  * @default false
+  //  */
+  // sortable: false,
+
+  // /**
+  //  * Whether the column is sorted.
+  //  *
+  //  * @property sorted
+  //  * @type Boolean
+  //  * @default false
+  //  */
+  // sorted: false,
+
+  // /**
+  //  * The sort direction when `sorted` is `true`. Either 'ascending' or 'descending'.
+  //  *
+  //  * @property direction
+  //  * @type String
+  //  */
+  // direction: '',
+
+  // /**
+  //  * Text alignment for the column header and respective cells. Possible
+  //  * values are `'left'`, `'center'` and `'right'`.
+  //  *
+  //  * @property align
+  //  * @type String
+  //  * @default 'left'
+  //  */
+  // align: 'left',
+
+  // /**
+  //  * Vertical alignment for the column header and respective cells. Possible
+  //  * values are `'top'`, `'middle'` and `'bottom'`.
+  //  *
+  //  * @property verticalAlign
+  //  * @type String
+  //  * @default 'top'
+  //  */
+  // verticalAlign: 'top'*/
+});
